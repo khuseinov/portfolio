cf2e51ecc34e22f68c5a153cf9b7dab455950002
Rustam <nirro-99@mail.ru>
2023-10-28 12:19:13 +0300

    add new config and styles

--- src/components/Main/Hero/Hero.tsx
+++ src/components/Main/Hero/Hero.tsx
@@ -17,7 +17,7 @@ const textAnimation = {
 	visible: (custom: number) => ({
 		y: 0,
 		opacity: 1,
-		transition: { delay: custom * 0.4, duration: 0.9 },
+		transition: { delay: custom * 0.3, duration: 1 },
 	}),
 }
 
